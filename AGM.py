dc7827c95af671112b51136029dcb590174ecba4
LittleHorus <47882271+LittleHorus@users.noreply.github.com>
2019-11-18 20:46:39 +0700

    add table

--- AGM.py
+++ AGM.py
@@ -3,11 +3,9 @@
 
 
 from PyQt5 import QtWidgets, QtCore, QtGui
+from PyQt5.QtCore import Qt
 import pyqtgraph as pg 
 import numpy as np
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-from matplotlib.figure import Figure
-import matplotlib.pyplot as plt
 import serial
 
 
@@ -25,12 +23,14 @@ class CommonWindow(QtWidgets.QWidget):
 		
 		#self.label = QtWidgets.QLabel("<b>S21</b> measure only regime")
 		#self.label.setAlignment(QtCore.Qt.AlignHCenter)
-
+		self.previous_row = 0
+		self.current_row = -1
 		self.record_number = 1
 		self.count = 0
 		#pg.setConfigOption('background', 'd')
 		pg.setConfigOption('foreground', 'g')	
 		self.graph = pg.PlotWidget()
+		self.graph.showGrid(1,1,1)
 		
 		self.graph.setMinimumSize(500,200)
 
@@ -41,7 +41,7 @@ class CommonWindow(QtWidgets.QWidget):
 		#r1.move(800,20)
 		#self.show()
 		
-		self.graph.plot(data, pen = pg.mkPen('g', width = 4), symbol = 'o', title = "Record №{}".format(self.record_number))
+		#self.graph.plot(data, pen = pg.mkPen('g', width = 4), symbol = 'o', title = "Record №{}".format(self.record_number))
 		#data = [2021,2025,2017,2018,2023,2026,2035,2058,2082,2134,2169,2224,2151,2113,2042,2021,2021,2021,2021,2021,2021,2021,2021,2021]#test data value for plot
 		
 			
@@ -61,14 +61,19 @@ class CommonWindow(QtWidgets.QWidget):
 		
 		self.onlyInt = QtGui.QIntValidator(1,1000)
 		#self.LineEdit.setValidator(self.onlyInt)
-
-
+		
+		
 		self.label_visa_connect = QtWidgets.QLabel("COM port:")
 		self.label_visa_connect.setMaximumSize(horizontal_size,vertical_size)
 		self.label_visa_connect.setSizePolicy(QtWidgets.QSizePolicy.Fixed,QtWidgets.QSizePolicy.Fixed)
 		self.btn_visa_connect = QtWidgets.QPushButton("Connect")
 		self.btn_visa_connect.setMaximumSize(horizontal_size,vertical_size)
 		self.btn_visa_connect.setSizePolicy(QtWidgets.QSizePolicy.Fixed,QtWidgets.QSizePolicy.Fixed)#Fixed
+		self.btn_visa_disconnect = QtWidgets.QPushButton("Disconnect")
+		self.btn_visa_disconnect.setMaximumSize(horizontal_size,vertical_size)
+		self.btn_visa_disconnect.setSizePolicy(QtWidgets.QSizePolicy.Fixed,QtWidgets.QSizePolicy.Fixed)#Fixed		
+		self.btn_visa_disconnect.setDisabled(True)
+		
 		
 		self.agm_serial_number = QtWidgets.QLineEdit("1")#center frequency for nwa
 		self.agm_serial_number.setMaximumSize(horizontal_size,vertical_size)
@@ -81,12 +86,13 @@ class CommonWindow(QtWidgets.QWidget):
 		self.agm_utc.setMaximumSize(horizontal_size,vertical_size)
 		self.agm_utc.setSizePolicy(QtWidgets.QSizePolicy.Fixed,QtWidgets.QSizePolicy.Fixed)
 
-		vertical_size = 30
-		horizontal_size = 200		
-		self.agm_recordbox = QtWidgets.QComboBox(self)#span for nwa
-		self.agm_recordbox.addItems(["№1  16.11.19  10:22:15", "№2  16.11.19  12:05:02", "№3  16.11.19  17:41:55"])
-		self.agm_recordbox.setMaximumSize(horizontal_size,vertical_size)
-		self.agm_recordbox.setSizePolicy(QtWidgets.QSizePolicy.Fixed,QtWidgets.QSizePolicy.Fixed)	
+		#vertical_size = 30
+		#horizontal_size = 200		
+		#self.agm_recordbox = QtWidgets.QComboBox(self)#span for nwa
+		#self.agm_recordbox.addItems(["№1  16.11.19  10:22:15", "№2  16.11.19  12:05:02", "№3  16.11.19  17:41:55"])
+		#self.agm_recordbox.setMaximumSize(horizontal_size,vertical_size)
+		#self.agm_recordbox.setSizePolicy(QtWidgets.QSizePolicy.Fixed,QtWidgets.QSizePolicy.Fixed)	
+		
 		vertical_size = 30
 		horizontal_size = 80
 		
@@ -98,9 +104,13 @@ class CommonWindow(QtWidgets.QWidget):
 		self.agm_filterbox.addItems(["50Hz", "60Hz"])
 		self.agm_filterbox.setMaximumSize(horizontal_size,vertical_size)
 		self.agm_filterbox.setSizePolicy(QtWidgets.QSizePolicy.Fixed,QtWidgets.QSizePolicy.Fixed)		
-		self.agm_filterapply = QtWidgets.QPushButton("Set\nFilter")
-		self.agm_filterapply.setMaximumSize(horizontal_size,vertical_size)
-		self.agm_filterapply.setSizePolicy(QtWidgets.QSizePolicy.Fixed,QtWidgets.QSizePolicy.Fixed)#Fixed
+		self.agm_filter_label = QtWidgets.QLabel("Filter:")
+		self.agm_filter_label.setMaximumSize(horizontal_size,vertical_size)
+		self.agm_filter_label.setSizePolicy(QtWidgets.QSizePolicy.Fixed,QtWidgets.QSizePolicy.Maximum)		
+		
+		#self.agm_filterapply = QtWidgets.QPushButton("Set\nFilter")
+		#self.agm_filterapply.setMaximumSize(horizontal_size,vertical_size)
+		#self.agm_filterapply.setSizePolicy(QtWidgets.QSizePolicy.Fixed,QtWidgets.QSizePolicy.Fixed)#Fixed
 		
 		self.agm_serial_number_label = QtWidgets.QLabel("AGM №:")
 		self.agm_serial_number_label.setMaximumSize(horizontal_size,vertical_size)
@@ -116,6 +126,36 @@ class CommonWindow(QtWidgets.QWidget):
 		#self.lbl_g.setMovie(self.movie)
 		#self.movie.start()
 		
+		self.table_of_records = QtWidgets.QTableWidget(self)
+		self.table_of_records.setColumnCount(4)
+		self.table_of_records.setMinimumSize(300,200)
+		self.table_of_records.setMaximumSize(320,700)
+		self.table_of_records.setRowCount(10)
+		self.table_of_records.setHorizontalHeaderLabels(["  Date  ", "  Time  ","Signal\nType","   GPS   "])
+		self.table_of_records.horizontalHeaderItem(0).setTextAlignment(QtCore.Qt.AlignCenter)
+		self.table_of_records.horizontalHeaderItem(1).setTextAlignment(QtCore.Qt.AlignCenter)
+		self.table_of_records.horizontalHeaderItem(2).setTextAlignment(QtCore.Qt.AlignCenter)
+		self.table_of_records.horizontalHeaderItem(3).setTextAlignment(QtCore.Qt.AlignCenter)
+		#self.table_of_records.setColumnWidth(2, 80)
+		self.table_of_records.horizontalHeader().setStretchLastSection(True)
+		
+		#data
+		self.table_of_records.setItem(0,0,QtWidgets.QTableWidgetItem("18.11.19"))
+		self.table_of_records.setItem(0,1,QtWidgets.QTableWidgetItem("21:56:17"))
+		self.table_of_records.setItem(0,2,QtWidgets.QTableWidgetItem("Mag"))
+		self.table_of_records.setItem(0,3,QtWidgets.QTableWidgetItem("54,575N\n82,579E"))
+		
+		self.table_of_records.setItem(1,0,QtWidgets.QTableWidgetItem("18.11.19"))
+		self.table_of_records.setItem(1,1,QtWidgets.QTableWidgetItem("21:59:22"))
+		self.table_of_records.setItem(1,2,QtWidgets.QTableWidgetItem("Mag"))
+		self.table_of_records.setItem(1,3,QtWidgets.QTableWidgetItem("54,575N\n82,579E"))
+
+		self.table_of_records.setItem(2,0,QtWidgets.QTableWidgetItem("18.11.19"))
+		self.table_of_records.setItem(2,1,QtWidgets.QTableWidgetItem("22:03:39"))
+		self.table_of_records.setItem(2,2,QtWidgets.QTableWidgetItem("Mag"))
+		self.table_of_records.setItem(2,3,QtWidgets.QTableWidgetItem("54,575N\n82,579E"))		
+		
+		self.table_of_records.resizeColumnsToContents()
 		
 		#self.agm_serial_number = QtWidgets.QLineEdit()
 		#self.form = QtWidgets.QFormLayout()
@@ -154,11 +194,14 @@ class CommonWindow(QtWidgets.QWidget):
 
 		
 		self.grid = QtWidgets.QGridLayout()
+		self.grid_2 = QtWidgets.QGridLayout()
+		self.grid_3 = QtWidgets.QGridLayout()
 		
 		
 		self.grid.addWidget(self.label_visa_connect, 0, 0)
 		self.grid.addWidget(self.comport_combo, 0, 1)
-		self.grid.addWidget(self.btn_visa_connect, 0, 2)		
+		self.grid.addWidget(self.btn_visa_connect, 0, 2)
+		self.grid.addWidget(self.btn_visa_disconnect, 0, 3)
 
 		self.grid.addWidget(self.agm_serial_number_label, 1, 0, alignment = QtCore.Qt.AlignCenter | QtCore.Qt.AlignCenter)
 		self.grid.addWidget(self.agm_utc_label, 2, 0, alignment = QtCore.Qt.AlignCenter | QtCore.Qt.AlignCenter)
@@ -170,33 +213,44 @@ class CommonWindow(QtWidgets.QWidget):
 		#self.grid.addWidget(self.btn_stop,2,1)
 		self.grid.addWidget(self.btn_save,4,1)
 		self.grid.addWidget(self.btn_clear,4,2)
-		self.grid.addWidget(self.agm_recordbox,6,1)
-		self.grid.addWidget(self.agm_recordbutton,6,0)
+		
+
+		
 		self.grid.addWidget(self.agm_filterbox,3,1)
-		self.grid.addWidget(self.agm_filterapply,3,0)		
+		self.grid.addWidget(self.agm_filter_label,3,0,alignment = QtCore.Qt.AlignCenter | QtCore.Qt.AlignCenter)		
 		#self.grid.addWidget(self.btn_pause,2,1)
 		
-		
-		
-		self.grid.addWidget(QtWidgets.QLabel(""),7,0)
-		self.grid.addWidget(QtWidgets.QLabel(""),7,1)
-		self.grid.addWidget(QtWidgets.QLabel(""),7,2)
-		self.grid.addWidget(QtWidgets.QLabel(""),7,3)
+		#self.grid_2.addWidget(self.agm_recordbox,0,1)#
+		#self.grid_2.addWidget(self.agm_recordbutton,0,0)# 		
+		self.grid_2.addWidget(QtWidgets.QLabel(""),6,0)
+		self.grid_2.addWidget(QtWidgets.QLabel(""),6,1)
+		self.grid_2.addWidget(QtWidgets.QLabel(""),6,2)
+		self.grid_2.addWidget(QtWidgets.QLabel(""),6,3)
+
+
+		self.grid.addWidget(QtWidgets.QLabel(""),6,0)
+		self.grid.addWidget(QtWidgets.QLabel(""),6,1)
+		self.grid.addWidget(QtWidgets.QLabel(""),6,2)
+		self.grid.addWidget(QtWidgets.QLabel(""),6,3)
 		
 		self.grid.addWidget(self.bar,5,0,1,5)
 		
+		self.grid_3.addWidget(self.table_of_records,0,0)
 		#self.grid.addWidget(self.lbl_g,10,0,1,5)
 		#self.grid.addWidget(self.lbl,9,0,20,10)#add picture to grid layout
 		#self.grid.addWidget(QtWidgets.QLabel(""),20,25)
 		#self.grid.addWidget(self.graph,5,10,5,5)#graph
 		#self.grid.setSpacing(10)
 		
-		self.grid.setHorizontalSpacing(5)
-		self.grid.setVerticalSpacing(5)
-		self.grid.setContentsMargins(5,5,5,5)
+		#self.grid_2.setHorizontalSpacing(5)
+		#self.grid_2.setVerticalSpacing(5)
+		#self.grid_2.setContentsMargins(5,5,5,5)
 				
 		self.vbox_1 = QtWidgets.QVBoxLayout()
 		self.vbox_1.insertLayout(0,self.grid)
+		self.vbox_1.insertLayout(1,self.grid_2)
+		self.vbox_1.insertLayout(2,self.grid_3)
+		self.vbox_1.insertStretch(3,0)
 		#self.vbox_1.insertLayout(1,self.form)
 		
 		#self.setLayout(self.grid)
@@ -230,11 +284,16 @@ class CommonWindow(QtWidgets.QWidget):
 		
 		#self.setLayout(self.vbox)
 
+		self.btn_load.setDisabled(True)
+		self.btn_save.setDisabled(True)
+		self.btn_clear.setDisabled(True)
 		
 		self.meas_thread = evThread()
 		
 		
-		#self.btn_stop.clicked.connect(self.on_interrupted)
+		self.btn_visa_connect.clicked.connect(self.on_connected)
+		self.btn_visa_disconnect.clicked.connect(self.on_disconnected)
+		
 		self.btn_load.clicked.connect(self.on_start_load)
 		#self.meas_thread.started.connect(self.on_meas_started)
 		self.meas_thread.finished.connect(self.on_meas_completed)
@@ -246,10 +305,23 @@ class CommonWindow(QtWidgets.QWidget):
 		#self.pwindow = paramWindow()
 		#self.btnSetCF.clicked.connect(self.on_choose_param)
 		
-		self.agm_recordbutton.clicked.connect(self.on_display_record)
+		#self.agm_recordbutton.clicked.connect(self.on_display_record)
+		self.table_of_records.itemClicked.connect(self.on_change_table_item)
 		
 		#self.comport_combo.activated.connect(self.on_activated_com_list)
+	def on_connected(self):
 		
+		self.btn_visa_connect.setDisabled(True)
+		self.btn_visa_disconnect.setDisabled(False)
+		self.btn_load.setDisabled(False)
+		#self.btn_save.setDisabled(True)
+		self.btn_clear.setDisabled(False)		
+	def on_disconnected(self):
+		self.btn_visa_connect.setDisabled(False)
+		self.btn_visa_disconnect.setDisabled(True)	
+		self.btn_load.setDisabled(True)
+		self.btn_save.setDisabled(True)
+		self.btn_clear.setDisabled(True)		
 		
 	def on_activated_com_list(self, str):
 		#self.label.setText(str)
@@ -274,7 +346,8 @@ class CommonWindow(QtWidgets.QWidget):
 		
 	def on_start_load(self):
 		self.btn_load.setDisabled(True)
-		
+		self.btn_save.setDisabled(True)
+		self.btn_clear.setDisabled(True)
 		#self.label.setText("measure started")
 		#self.graph.clear()
 		self.bar.setValue(0)
@@ -283,19 +356,37 @@ class CommonWindow(QtWidgets.QWidget):
 		
 	def on_meas_completed(self):
 		self.btn_load.setDisabled(False)
+		self.btn_save.setDisabled(False)
+		self.btn_clear.setDisabled(False)		
 		#self.btn_save.setDisable(False)
+		
+	def on_save(self):
+		pass
 	def on_interrupted(self):
 		self.meas_thread.running = False
 		
 	def on_display_record(self):
-		data = [1021,1025,1017,1029,1023,1026,1035,1058,1082,1134,1169,1224,1151,1113,1042,1021,1021,1021,1021,1021,1013,1018,1004,1002]#test data value for plot
+		data = [2018,2025,2017,2029,2022,2026,2035,2058,2082,2134,2169,2224,2151,2113,2042,2021,2021,2024,2021,2021,2016,2013,2007,2001]#test data value for plot
 		for i in range(len(data)):
-			data += np.random.normal(0,10,len(data))
+			data += np.random.normal(0,4,len(data))
 		self.graph.clear()
-		self.record_number = self.agm_recordbox.currentIndex()
+		self.record_number = self.current_row + 1
 		self.graph.plot(data, pen = pg.mkPen('g', width = 4), symbol = 't', title = "Record №{}".format(self.record_number))
 		self.graph.showGrid(1,1,1)
-				
+	
+	def on_change_table_item(self, item):
+		self.previous_row = self.current_row
+		self.current_row = item.row()
+		for j in range(4):
+			self.table_of_records.item(self.current_row, j).setBackground(QtGui.QColor(100,200,50))
+			if self.previous_row != -1:
+				self.table_of_records.item(self.previous_row, j).setBackground(QtGui.QColor(255,255,255))
+		if  self.current_row != self.previous_row:
+			self.on_display_record()
+		
+		
+
+	
 		
 	def closeEvent(self, event):#перехватываем событие закрытия приложения
 		result = QtWidgets.QMessageBox.question(self, "Подтверждение закрытия окна", "Вы действительно хотите закрыть окно?", QtWidgets.QMessageBox.Yes | QtWidgets.QMessageBox.No, QtWidgets.QMessageBox.No )
@@ -308,13 +399,7 @@ class CommonWindow(QtWidgets.QWidget):
 		else:
 			event.ignore()
 		
-class paramWindow(QtWidgets.QWidget):
-	"""класс для окон с настройкой параметров"""
-	def __init__(self, parent = None):
-		QtWidgets.QWidget.__init__(self, parent)
-		self.label_cf = QtWidgets.QLabel("AGM idn:")
-		self.label_cf.setAlignment(QtCore.Qt.AlignHCenter)
-		
+
 
 class evThread(QtCore.QThread):
 	
@@ -328,12 +413,12 @@ class evThread(QtCore.QThread):
 		
 	def run(self):
 		self.running = True
-		for i in range(100):
+		for i in range(25):
 			if self.running == True:
 				self.sleep(1)
 				self.status_signal.emit("{} / {}".format(i+1,100))
 				self.dataplot.emit(np.random.randn(200,))
-				self.progress.emit(i+1)
+				self.progress.emit(4*i+4)
 				
 		if self.running == False:
 			self.status_signal.emit("Interrupted")
