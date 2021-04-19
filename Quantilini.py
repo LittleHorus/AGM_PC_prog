93f80f23530852a2e2e7e34a6c120d5826d27ccf
LittleHorus <47882271+LittleHorus@users.noreply.github.com>
2021-04-19 13:39:57 +0700

    clear

--- Quantilini.py
+++ Quantilini.py
@@ -143,47 +143,12 @@ class CommonWindow(QtWidgets.QWidget):
 		sh1 = gl.GLLinePlotItem(pos=pts, width = 4, antialias = False, mode = 'line_strip', color = (0.0, 1.0, 0.0, 1.0))
 		self.view.addItem(sh1)
 
-		self.md = gl.MeshData.sphere(rows=20, cols=40)
+		self.md = gl.MeshData.sphere(rows=20, cols=40,radius=[1])
 		self.m1 = gl.GLMeshItem(meshdata=self.md,smooth=True,color=(0.5, 0, 0.5, 0.2),shader="balloon",glOptions="additive")
 		self.view.addItem(self.m1)		
 
-
-
-		self.INITIAL_MODBUS = 0xFFFF
-		self.INITIAL_DF1 = 0x0000
-		self.table = (
-		0x0000, 0xC0C1, 0xC181, 0x0140, 0xC301, 0x03C0, 0x0280, 0xC241,
-		0xC601, 0x06C0, 0x0780, 0xC741, 0x0500, 0xC5C1, 0xC481, 0x0440,
-		0xCC01, 0x0CC0, 0x0D80, 0xCD41, 0x0F00, 0xCFC1, 0xCE81, 0x0E40,
-		0x0A00, 0xCAC1, 0xCB81, 0x0B40, 0xC901, 0x09C0, 0x0880, 0xC841,
-		0xD801, 0x18C0, 0x1980, 0xD941, 0x1B00, 0xDBC1, 0xDA81, 0x1A40,
-		0x1E00, 0xDEC1, 0xDF81, 0x1F40, 0xDD01, 0x1DC0, 0x1C80, 0xDC41,
-		0x1400, 0xD4C1, 0xD581, 0x1540, 0xD701, 0x17C0, 0x1680, 0xD641,
-		0xD201, 0x12C0, 0x1380, 0xD341, 0x1100, 0xD1C1, 0xD081, 0x1040,
-		0xF001, 0x30C0, 0x3180, 0xF141, 0x3300, 0xF3C1, 0xF281, 0x3240,
-		0x3600, 0xF6C1, 0xF781, 0x3740, 0xF501, 0x35C0, 0x3480, 0xF441,
-		0x3C00, 0xFCC1, 0xFD81, 0x3D40, 0xFF01, 0x3FC0, 0x3E80, 0xFE41,
-		0xFA01, 0x3AC0, 0x3B80, 0xFB41, 0x3900, 0xF9C1, 0xF881, 0x3840,
-		0x2800, 0xE8C1, 0xE981, 0x2940, 0xEB01, 0x2BC0, 0x2A80, 0xEA41,
-		0xEE01, 0x2EC0, 0x2F80, 0xEF41, 0x2D00, 0xEDC1, 0xEC81, 0x2C40,
-		0xE401, 0x24C0, 0x2580, 0xE541, 0x2700, 0xE7C1, 0xE681, 0x2640,
-		0x2200, 0xE2C1, 0xE381, 0x2340, 0xE101, 0x21C0, 0x2080, 0xE041,
-		0xA001, 0x60C0, 0x6180, 0xA141, 0x6300, 0xA3C1, 0xA281, 0x6240,
-		0x6600, 0xA6C1, 0xA781, 0x6740, 0xA501, 0x65C0, 0x6480, 0xA441,
-		0x6C00, 0xACC1, 0xAD81, 0x6D40, 0xAF01, 0x6FC0, 0x6E80, 0xAE41,
-		0xAA01, 0x6AC0, 0x6B80, 0xAB41, 0x6900, 0xA9C1, 0xA881, 0x6840,
-		0x7800, 0xB8C1, 0xB981, 0x7940, 0xBB01, 0x7BC0, 0x7A80, 0xBA41,
-		0xBE01, 0x7EC0, 0x7F80, 0xBF41, 0x7D00, 0xBDC1, 0xBC81, 0x7C40,
-		0xB401, 0x74C0, 0x7580, 0xB541, 0x7700, 0xB7C1, 0xB681, 0x7640,
-		0x7200, 0xB2C1, 0xB381, 0x7340, 0xB101, 0x71C0, 0x7080, 0xB041,
-		0x5000, 0x90C1, 0x9181, 0x5140, 0x9301, 0x53C0, 0x5280, 0x9241,
-		0x9601, 0x56C0, 0x5780, 0x9741, 0x5500, 0x95C1, 0x9481, 0x5440,
-		0x9C01, 0x5CC0, 0x5D80, 0x9D41, 0x5F00, 0x9FC1, 0x9E81, 0x5E40,
-		0x5A00, 0x9AC1, 0x9B81, 0x5B40, 0x9901, 0x59C0, 0x5880, 0x9841,
-		0x8801, 0x48C0, 0x4980, 0x8941, 0x4B00, 0x8BC1, 0x8A81, 0x4A40,
-		0x4E00, 0x8EC1, 0x8F81, 0x4F40, 0x8D01, 0x4DC0, 0x4C80, 0x8C41,
-		0x4400, 0x84C1, 0x8581, 0x4540, 0x8701, 0x47C0, 0x4680, 0x8641,
-		0x8201, 0x42C0, 0x4380, 0x8341, 0x4100, 0x81C1, 0x8081, 0x4040 )
+		self.view.setCameraPosition(distance=10, azimuth=-90)
+		self.view.setWindowTitle('3D plot label')
 
 		self.ComPort = str
 		self.comport_combo = QtWidgets.QComboBox()
@@ -197,10 +162,6 @@ class CommonWindow(QtWidgets.QWidget):
 		
 		self.onlyInt = QtGui.QIntValidator(1,5000)
 		
-		self.btn_cord_fixed = QtWidgets.QPushButton("&Capture")
-		self.btn_cord_fixed.setMaximumSize(120,60)
-		self.btn_cord_fixed.setSizePolicy(QtWidgets.QSizePolicy.Fixed,QtWidgets.QSizePolicy.Fixed)
-
 		self.btn_load_file = QtWidgets.QPushButton("&Load File")
 		self.btn_load_file.setMaximumSize(80,60)
 		self.btn_load_file.setSizePolicy(QtWidgets.QSizePolicy.Fixed,QtWidgets.QSizePolicy.Fixed)	
@@ -253,7 +214,6 @@ class CommonWindow(QtWidgets.QWidget):
 		
 		self.grid.addWidget(self.timeout_label, 1, 3)
 		self.grid.addWidget(self.btn_fetch,2,0)
-		#self.grid.addWidget(self.btn_stop,2,1)
 		self.grid.addWidget(self.btn_save,2,1)
 		self.grid.addWidget(self.btn_load_file, 2,2)
 		self.grid.addWidget(self.data_fetch_timeout, 2,3)
@@ -261,11 +221,6 @@ class CommonWindow(QtWidgets.QWidget):
 		self.grid.addWidget(self.description_widget, 3,0,4,5)
 		self.grid.addWidget(self.log_widget, 8, 0, 7, 5)
 
-		self.grid_plot_labels.addWidget(self.btn_cord_fixed, 0,0)
-		self.grid_plot_labels.addWidget(QtWidgets.QLabel(""),0,3)
-		self.grid_plot_labels.addWidget(QtWidgets.QLabel(""),0,5)
-		#self.grid_plot_labels.insertStretch(0,4)
-			
 		self.grid.addWidget(QtWidgets.QLabel(""),15,0)
 		self.grid.addWidget(QtWidgets.QLabel(""),15,1)
 		self.grid.addWidget(QtWidgets.QLabel(""),15,2)
@@ -279,20 +234,14 @@ class CommonWindow(QtWidgets.QWidget):
 		self.vbox_1 = QtWidgets.QVBoxLayout()
 		self.vbox_1.insertLayout(0,self.grid)
 		self.vbox_1.insertLayout(1,self.grid_2)
-		#self.vbox_1.insertLayout(2,self.grid_3)#table grid
-		#self.vbox_1.addWidget(self.agm_readblock)
 		self.vbox_1.insertStretch(3,0)
-		#self.vbox_1.insertLayout(1,self.form)
-		#self.setLayout(self.grid)
 		
 		self.hbox = QtWidgets.QHBoxLayout()
 		self.vbox_graph_table = QtWidgets.QVBoxLayout()
 
-		#self.vbox_graph_table.insertLayout(0,self.grid_plot_labels)
 		self.vbox_graph_table.addWidget(self.graph)
 		self.vbox_graph_table.addWidget(self.view)
 		self.vbox_graph_table.insertStretch(2,0)
-		#self.hbox.addWidget(self.m)
 		self.hbox.insertLayout(0,self.vbox_1)
 		self.hbox.insertLayout(1,self.vbox_graph_table)
 	
@@ -300,15 +249,10 @@ class CommonWindow(QtWidgets.QWidget):
 
 		self.hbox_2.insertSpacing(0,335)
 		self.hbox_2.insertStretch(2,0)
-		#self.hbox_upper.addSpacing(200)
 		
 		self.vbox = QtWidgets.QVBoxLayout()
-		#self.vbox.addWidget(self.label)		
-		#self.vbox.insertLayout(0,self.hbox1)
 		self.vbox.insertLayout(0,self.hbox)
 		self.vbox.insertLayout(1,self.hbox_2)
-		#self.vbox.insertLayout(2,self.grid_3)
-		#self.vbox.addWidget(self.table_of_records,1)
 		self.setLayout(self.vbox)
 
 		self.btn_fetch.setDisabled(True)
@@ -322,15 +266,6 @@ class CommonWindow(QtWidgets.QWidget):
 		self.btn_save.clicked.connect(self.on_save_to_file)
 		self.btn_load_file.clicked.connect(self.on_load_from_file) 
 		self.btn_fetch.clicked.connect(self.on_fetch_data)
-
-		#self.meas_thread.started.connect(self.on_meas_started)
-		#self.meas_thread.finished.connect(self.on_meas_completed)
-		#self.meas_thread.status_signal.connect(self.on_status_text_change, QtCore.Qt.QueuedConnection)
-		#self.meas_thread.dataplot.connect(self.graph.plot, QtCore.Qt.QueuedConnection)
-		#self.meas_thread.progress.connect(self.on_progress_go,QtCore.Qt.QueuedConnection)
-		#self.proxy = pg.SignalProxy(self.graph.scene().sigMouseMoved, rateLimit=60, slot=self.mouseMoved)
-		#self.curve.sigClicked.connect(self.clicked_point)
-		#self.curve.sigPointsClicked.connect(self.clicked_point)
 
 	def on_connected(self):
 		try:
